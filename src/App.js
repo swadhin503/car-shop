956892523c7c1b57ad8a4adf98faaa2986b76a2a
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-03-26 16:49:40 +0600

    css half complte

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Shop from './components/Shop/Shop';
 function App() {
   return (
     <div>
+      <h1 className="header-title"><span className="span-title">Swadhin</span> Car Shop</h1>
       <Shop></Shop>
     </div>
   );
--- src/components/Shop/Car/Car.js
+++ src/components/Shop/Car/Car.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Car.css'
 
 const Car = (props) => {
     console.log(props.car)
-    const {name,id,img,price} = props.car;
+    const {name,img,price} = props.car;
     return (
         <div className="car-card">
             <img src={img} alt="" />
-            <h2>Showing Cars: {name}</h2>
+            <h2>{name}</h2>
+            <p>Price: ${price}</p>
+            <button className="card-btn">Add To Cart</button>
+
         </div>
     );
 };
